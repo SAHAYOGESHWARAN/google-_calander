d8f4abd92deabbb584f4db7ed4b3d6044d43cb59
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-30 09:43:00 +0530

    google calander

---
+++ backend/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    },
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+  };
+  
